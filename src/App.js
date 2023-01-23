b5e90551ba0c278aafeae0de979cd4cc9083d354
Tom Booth <tombeezle@gmail.com>
2023-01-23 15:00:40 -0800

    built out home, and added paths to the diff components, made return links functional, book details get id

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import BookList from './components/book/BookList';
 import './App.css';
+import { Route, Switch } from 'react-router-dom';
+import Home from './components/Home.js';
+import BookDetail from './components/book/BookDetail.js';
 
 function App() {
   // TODO: Add routes to books & views
   return (
     <main className="container">
-      <h1>Library Catalog</h1>
-      <BookList />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/books" component={BookList} />
+        <Route path="/book/:id" component={BookDetail} />
+      </Switch>
     </main>
   );
 }
